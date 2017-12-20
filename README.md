1a80f480c240287bc25489052db59ac73a8894d9
Aleksas Pielikis <ant.kampo@gmail.com>
2017-12-20 18:45:42 +0200

    PermissionsHelper added.

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # androidGlesCamera2
 Handle camera2 preview drawing. 
 
-Code is borrowed from http://maninara.blogspot.lt/2015/03/render-camera-preview-using-opengl-es.html .
+References:
+* http://maninara.blogspot.lt/2015/03/render-camera-preview-using-opengl-es.html
+* https://github.com/googlecreativelab/shadercam/
+
--- app/src/main/java/com/gscoder/androidglescamera2/MainActivity.java
+++ app/src/main/java/com/gscoder/androidglescamera2/MainActivity.java
@@ -1,13 +1,22 @@
 package com.gscoder.androidglescamera2;
 
+import android.Manifest;
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import java.util.Arrays;
+
+public class MainActivity extends FragmentActivity implements PermissionsHelper.PermissionsListener {
+    private static final String TAG = MainActivity.class.getSimpleName();
     private CameraGLSurfaceView mView;
+    private PermissionsHelper mPermissionsHelper;
+    private boolean mPermissionsSatisfied = false;
 
     @Override
     public void onCreate ( Bundle savedInstanceState) {
@@ -19,12 +28,22 @@ public class MainActivity extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         mView = new CameraGLSurfaceView(this);
         setContentView ( mView );
+
+        if(PermissionsHelper.isMorHigher())
+            setupPermissions();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         mView.onResume();
+
+        if(PermissionsHelper.isMorHigher() && !mPermissionsSatisfied) {
+            if(!mPermissionsHelper.checkPermissions())
+                return;
+            else
+                mPermissionsSatisfied = true; //extra helper as callback sometimes isnt quick enough for future results
+        }
     }
 
     @Override
@@ -33,4 +52,25 @@ public class MainActivity extends Activity {
         super.onPause();
     }
 
+    private void setupPermissions() {
+        mPermissionsHelper = PermissionsHelper.attach(this);
+        mPermissionsHelper.setRequestedPermissions(
+                Manifest.permission.CAMERA
+        );
+    }
+
+    @Override
+    public void onPermissionsSatisfied() {
+        Log.d(TAG, "onPermissionsSatisfied()");
+        mPermissionsSatisfied = true;
+
+    }
+
+    @Override
+    public void onPermissionsFailed(String[] failedPermissions) {
+        Log.e(TAG, "onPermissionsFailed()" + Arrays.toString(failedPermissions));
+        mPermissionsSatisfied = false;
+        Toast.makeText(this, "shadercam needs all permissions to function, please try again.", Toast.LENGTH_LONG).show();
+        this.finish();
+    }
 }

1e8733897fbb3e533bc254bc0126bcd490301285
Aleksas Pielikis <ant.kampo@gmail.com>
2017-12-21 15:41:07 +0200

    Add GL error handling.

--- app/src/main/java/com/gscoder/androidglescamera2/CameraViewRenderer.java
+++ app/src/main/java/com/gscoder/androidglescamera2/CameraViewRenderer.java
@@ -14,9 +14,12 @@ import java.nio.FloatBuffer;
 import javax.microedition.khronos.opengles.GL10;
 
 public class CameraViewRenderer implements GLSurfaceView.Renderer, SurfaceTexture.OnFrameAvailableListener {
+    private static final String TAG = CameraViewRenderer.class.getSimpleName();
+
     private final String cameraVertexShader = "" +
             "attribute vec2 vPosition;\n" +
             "attribute vec2 vTexCoord;\n" +
+            "uniform mat4 uTexMatrix;\n" +
             "varying vec2 texCoord;\n" +
             "void main() {\n" +
             "  texCoord = vTexCoord;\n" +
@@ -39,6 +42,8 @@ public class CameraViewRenderer implements GLSurfaceView.Renderer, SurfaceTextur
 
     private boolean mGLInit = false;
     private boolean mUpdateSurfaceTexture = false;
+
+    private float[] mTexMatrix = new float[] {1, 0, 0, 0,   0, 1, 0, 0,   0, 0, 1, 0,   0, 0, 0, 1};
 
     CameraGLSurfaceView mView;
     CameraHandler mCameraHandler;
@@ -91,6 +96,7 @@ public class CameraViewRenderer implements GLSurfaceView.Renderer, SurfaceTextur
         mCameraHandler.setSurfaceTexture(mSurfaceTexture);
 
         GLES20.glClearColor(1.0f, 1.0f, 1.0f, 1.0f); // Clear white
+        checkGlError("glClearColor");
 
         hProgram = loadShader (cameraVertexShader, cameraFragmentShader);
 
@@ -106,6 +112,7 @@ public class CameraViewRenderer implements GLSurfaceView.Renderer, SurfaceTextur
     public void onDrawFrame ( GL10 unused ) {
         if ( !mGLInit ) return;
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        checkGlError("glClear");
 
         synchronized(this) {
             if (mUpdateSurfaceTexture) {
@@ -115,35 +122,72 @@ public class CameraViewRenderer implements GLSurfaceView.Renderer, SurfaceTextur
         }
 
         GLES20.glUseProgram(hProgram);
+        checkGlError("glUseProgram");
+
+        int tmh = GLES20.glGetUniformLocation ( hProgram, "uTexMatrix" );
+        checkGlError("glGetUniformLocation");
+
+        GLES20.glUniformMatrix4fv(tmh, 1, false, mTexMatrix, 0);
+        checkGlError("glUniformMatrix4fv");
 
         int ph = GLES20.glGetAttribLocation(hProgram, "vPosition");
+        checkGlError("glGetAttribLocation");
+
         int tch = GLES20.glGetAttribLocation ( hProgram, "vTexCoord" );
+        checkGlError("glGetAttribLocation");
 
         GLES20.glVertexAttribPointer(ph, 2, GLES20.GL_FLOAT, false, 4*2, pVertex);
+        checkGlError("glVertexAttribPointer");
+
         GLES20.glVertexAttribPointer(tch, 2, GLES20.GL_FLOAT, false, 4*2, pTexCoord );
+        checkGlError("glVertexAttribPointer");
+
         GLES20.glEnableVertexAttribArray(ph);
+        checkGlError("glEnableVertexAttribArray");
+
         GLES20.glEnableVertexAttribArray(tch);
+        checkGlError("glEnableVertexAttribArray");
 
         GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
+        checkGlError("glActiveTexture");
+
         GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, hTex[0]);
+        checkGlError("glBindTexture");
+
         GLES20.glUniform1i(GLES20.glGetUniformLocation ( hProgram, "sTexture" ), 0);
+        checkGlError("glUniform1i");
 
         GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4);
+        checkGlError("glDrawArrays");
+
         GLES20.glFlush();
+        checkGlError("glFlush");
     }
 
     public void onSurfaceChanged (GL10 unused, int width, int height ) {
         GLES20.glViewport(0, 0, width, height);
+        checkGlError("glViewport");
     }
 
     private void initTex() {
         hTex = new int[1];
         GLES20.glGenTextures ( 1, hTex, 0 );
+        checkGlError("glGenTextures");
+
         GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, hTex[0]);
+        checkGlError("glBindTexture");
+
         GLES20.glTexParameteri(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE);
+        checkGlError("glTexParameteri");
+
         GLES20.glTexParameteri(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE);
+        checkGlError("glTexParameteri");
+
         GLES20.glTexParameteri(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_NEAREST);
+        checkGlError("glTexParameteri");
+
         GLES20.glTexParameteri(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_NEAREST);
+        checkGlError("glTexParameteri");
     }
 
     public synchronized void onFrameAvailable ( SurfaceTexture st ) {
@@ -153,33 +197,71 @@ public class CameraViewRenderer implements GLSurfaceView.Renderer, SurfaceTextur
 
     private static int loadShader ( String vss, String fss ) {
         int vshader = GLES20.glCreateShader(GLES20.GL_VERTEX_SHADER);
+        checkGlError("glCreateShader");
+
         GLES20.glShaderSource(vshader, vss);
+        checkGlError("glShaderSource");
+
         GLES20.glCompileShader(vshader);
+        checkGlError("glCompileShader");
+
         int[] compiled = new int[1];
         GLES20.glGetShaderiv(vshader, GLES20.GL_COMPILE_STATUS, compiled, 0);
         if (compiled[0] == 0) {
+            checkGlError("glGetShaderiv");
+
             Log.e("Shader", "Could not compile vertex shader");
             Log.v("Shader", "Could not compile vertex shader:"+GLES20.glGetShaderInfoLog(vshader));
             GLES20.glDeleteShader(vshader);
+            checkGlError("glDeleteShader");
+
             vshader = 0;
         }
 
         int fshader = GLES20.glCreateShader(GLES20.GL_FRAGMENT_SHADER);
+        checkGlError("glCreateShader");
+
         GLES20.glShaderSource(fshader, fss);
+        checkGlError("glShaderSource");
+
         GLES20.glCompileShader(fshader);
+        checkGlError("glCompileShader");
+
         GLES20.glGetShaderiv(fshader, GLES20.GL_COMPILE_STATUS, compiled, 0);
+        checkGlError("glGetShaderiv");
+
         if (compiled[0] == 0) {
             Log.e("Shader", "Could not compile fragment shader");
             Log.v("Shader", "Could not compile fragment shader:"+GLES20.glGetShaderInfoLog(fshader));
+
             GLES20.glDeleteShader(fshader);
+            checkGlError("glDeleteShader");
+
             fshader = 0;
         }
 
         int program = GLES20.glCreateProgram();
+        checkGlError("glCreateProgram");
+
         GLES20.glAttachShader(program, vshader);
+        checkGlError("glAttachShader");
+
         GLES20.glAttachShader(program, fshader);
+        checkGlError("glAttachShader");
+
         GLES20.glLinkProgram(program);
+        checkGlError("glLinkProgram");
+
 
         return program;
+    }
+
+    public static void checkGlError(String op) {
+        int error = GLES20.glGetError();
+        if (error != GLES20.GL_NO_ERROR) {
+            String msg = op + ": glError 0x" + Integer.toHexString(error);
+            Log.e(TAG, msg);
+            throw new RuntimeException(msg);
+        }
     }
 }
